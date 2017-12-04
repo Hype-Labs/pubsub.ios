3e96f9eeca72ef3338581ed2a5385041344be5e5
Xavier Araújo <xa@hypelabs.io>
2017-12-04 10:05:00 +0000

    Added empty XCode project + HypeSDK

---
+++ HypePubSub.iOS/HypePubSub.iOS/Client.swift
@@ -0,0 +1,13 @@
+//
+//  Client.swift
+//  HypePubSub.iOS
+//
+//  Created by Xavier Araújo on 04/12/2017.
+//  Copyright © 2017 Xavier Araújo. All rights reserved.
+//
+
+import Foundation
+
+class Client {
+    
+}
