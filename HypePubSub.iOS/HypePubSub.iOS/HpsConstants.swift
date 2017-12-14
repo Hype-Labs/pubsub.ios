df33efc5fc61ea679d4341aff382bd5e1770e537
Xavier Araújo <xa@hypelabs.io>
2017-12-14 18:11:25 +0000

    Changed line position

--- HypePubSub.iOS/HypePubSub.iOS/HpsConstants.swift
+++ HypePubSub.iOS/HypePubSub.iOS/HpsConstants.swift
@@ -9,12 +9,11 @@ class HpsConstants
     static let ENCODING_STANDARD = String.Encoding.utf8
     static let LOG_PREFIX = " :: HpsApplication :: "
     static let NOTIFICATIONS_TITLE = "HypePubSub"
+    static let STANDARD_HYPE_SERVICES = ["hype-jobs", "hype-sports", "hype-news", "hype-weather", "hype-music", "hype-movies"]
     
     static let NOTIFICATION_CLIENTS_VIEW_CONTROLLER = "refreshClientsViewController"
     static let NOTIFICATION_SUBSCRIPTIONS_VIEW_CONTROLLER = "refreshSubscriptionsViewController"
     static let NOTIFICATION_MESSAGES_VIEW_CONTROLLER = "refreshMessagesViewController-"
     static let NOTIFICATION_SERVICE_MANAGERS_VIEW_CONTROLLER = "refreshServiceManagersViewController"
     static let NOTIFICATION_SUBSCRIBERS_VIEW_CONTROLLER = "refreshSubscribersViewController-"
-    
-    static let STANDARD_HYPE_SERVICES = ["hype-jobs", "hype-sports", "hype-news", "hype-weather", "hype-music", "hype-movies"]
 }
